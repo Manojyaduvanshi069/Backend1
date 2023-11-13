881194818f13c6124fd1c5fcccf7682bf487da1b
Manoj <manojyaduvanshi9811@gmail.com>
2023-11-13 13:36:43 +0530

    first commit

---
+++ math.js
@@ -0,0 +1,15 @@
+//////// module.exports="Hello!";
+
+const mul=(a,b)=>a*b;
+const sum=(a,b)=>a+b;
+const PI=3.14;
+const g=9.8;
+
+let obj ={
+    mul:mul,
+    sum:sum,
+    PI:PI,
+    g:g,
+};
+
+module.exports=obj;
---
+++ script.js
@@ -0,0 +1,17 @@
+// let n=5;
+
+// for(let i=0; i<n; i++){
+//     console.log("Hello",i);
+// }
+
+// // console.log(process.argv);
+
+/////module.exports////////////////
+
+// let math=require("./math");
+
+// console.log(math);
+// console.log(math.sum(2,4));
+
+let info=require("./Fruits");
+console.log(info);
